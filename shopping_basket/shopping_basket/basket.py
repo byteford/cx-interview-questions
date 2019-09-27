115e4ed22179a074319f274ef42057dabd8c0f96
James Sandford <sandford.james@gmail.com>
2019-09-27 14:17:05 +0100

    added comments to say what type of variable the functions need

--- shopping_basket/shopping_basket/basket.py
+++ shopping_basket/shopping_basket/basket.py
@@ -3,16 +3,18 @@ class basket():
     _catalog = {}
     _offers = {}
     #update stored catalog and/ or offers
+    #catalog    : Dict[string, float] (name, cost)
+	#offers 	: Dict[string, offer] (name, offer object)
     def update(self, catalog = None, offers = None):
         if catalog != None:
             self._catalog = catalog
         if offers != None:
-            self._offers = offers
-    #clear any stored items in the basket
+            self._offers = offers 
     def clearBasket(self):
         self._items = {}
         pass
     #add an item of any quantity
+    #name	    : string
     def addItem(self, name, amount = 1):
         if( name in self._items):
             self._items[name] += amount
@@ -20,12 +22,17 @@ class basket():
             self._items[name] = amount
         pass
     #add a Dict of items to the existing basket
+    #amount	    : int
+    #items 	    : Dict[string,int] (name, amount)
     def addItems(self, items):
         for name,amount in items.items():
             self.addItem(name,amount)
         pass
-    # calculates the value, returns:
-    # SubTotal, Discount, Total
+    # calculates the value
+    #catalog    : Dict[string, float] (name, cost)
+	#offers 	: Dict[string, offer] (name, offer object)
+	#items 	    : Dict[string,int] (name, amount)
+	#returns subtotal, discount, total : float
     # can also replace any stored values (DOES NOT ADD TO)
     def calc(self, catalog=None, offers=None, items = None):
         #inits variables
@@ -47,7 +54,7 @@ class basket():
             return subTotal, discount, total
         if items != None:
             self._items = items
-        subTotal = self.calSubTotal(self._catalog,self._items)
+        subTotal = self.calcSubTotal(self._catalog,self._items)
         if subTotal < 0:
             return 0,0,0
         if self._offers:
@@ -55,8 +62,11 @@ class basket():
         
         total = subTotal - discount
         return subTotal, round(discount,2), round(total,2)
-
-    def calSubTotal(self,catalog, items):
+    #calculates a subtotal from a catalog and list of items
+    #catalog    : Dict[string, float] (name, cost)
+	#items 	    : Dict[string,int] (name, amount)
+	#returns subtotal: float
+    def calcSubTotal(self,catalog, items):
         subTotal = 0
         for key, value in items.items():
             if not key in catalog:
@@ -79,6 +89,11 @@ class basket():
                     return 0
                 subTotal += catalog[key] * value
         return subTotal
+    #Calculates how much discount to give based only on sigle product offers	
+    #catalog    : Dict[string, float] (name, cost)
+	#offers 	: Dict[string, offer] (name, offer object)
+	#items 	    : Dict[string,int] (name, amount)
+	#returns discount: float
     def calcSingleOffers(self, catalog, offers, items):
         discount = 0
         for key, value in items.items():

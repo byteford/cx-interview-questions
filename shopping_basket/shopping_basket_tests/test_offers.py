c6ad0b39712af6241bdd9da9bdc296b1b94d1ae4
James Sandford <sandford.james@gmail.com>
2019-09-27 14:45:58 +0100

    set up tests for buy x get cheapest

--- shopping_basket/shopping_basket_tests/test_offers.py
+++ shopping_basket/shopping_basket_tests/test_offers.py
@@ -14,5 +14,13 @@ class TestOffers(unittest.TestCase):
     def test_percent(self):
         per = offers.percent(percent=25)
         self.assertEqual(per.discount(amount=2,cost=1.89), .94)
+    def test_xgetCheap(self):
+        cheap = offers.buyxGetCheap(amount=3)
+        cat = {"Shampoo (Large)": 3.50}
+        self.assertEqual(cheap.discount(catalog=cat,items={"Shampoo (Large)":3}),3.50)
+    def test_xgetCheapmulti(self):
+        cheap = offers.buyxGetCheap(amount=3)
+        cat = {"Shampoo (Small)": 2.00, "Shampoo (Medium)": 2.50, "Shampoo (Large)": 3.50}
+        self.assertEqual(cheap.discount(catalog=cat,items={"Shampoo (Large)":3, "Shampoo (Medium)":2,"Shampoo (Small)":1}),5.50)
 if __name__ == '__main__':
     unittest.main()

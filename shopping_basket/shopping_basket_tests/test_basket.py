8dbd8c008cbf867cb6f4974829cdeb0b65bb939f
James Sandford <sandford.james@gmail.com>
2019-09-27 10:32:52 +0100

    added catalog and tests to check item maths without discounts

--- shopping_basket/shopping_basket_tests/test_basket.py
+++ shopping_basket/shopping_basket_tests/test_basket.py
@@ -2,17 +2,28 @@ import unittest
 from shopping_basket import basket
 
 class TestBasket(unittest.TestCase):
-    bask = basket.basket()
+    _bask = basket.basket()
+    _cat = {"Baked Beans": 0.99,"Biscuits": 1.20, "Sardines": 1.89, "Shampoo (Small)": 2.00, "Shampoo (Medium)": 2.50, "Shampoo (Large)": 3.50}
     def test_allNone(self):
-        sub,dis,total = self.bask.calc()
+        sub,dis,total = self._bask.calc()
         self.assertEqual(sub,0)
         self.assertEqual(dis,0)
         self.assertEqual(total,0)
     def test_catNone(self):
-        sub,dis,total = self.bask.calc(items = {"Baked Beans":4})
+        sub,dis,total = self._bask.calc(items = {"Baked Beans":4})
         self.assertEqual(sub,0)
         self.assertEqual(dis,0)
         self.assertEqual(total,0)
         pass
+    def test_1itemNoOffer(self):
+        sub,dis,total = self._bask.calc(catalog = self._cat, items = {"Baked Beans":4})
+        self.assertEqual(sub,3.96)
+        self.assertEqual(dis,0)
+        self.assertEqual(total,3.96)
+    def test_itemsNoOffer(self):
+        sub,dis,total = self._bask.calc(catalog = self._cat, items = {"Baked Beans":4, "Biscuits": 1})
+        self.assertEqual(sub,5.16)
+        self.assertEqual(dis,0)
+        self.assertEqual(total,5.16)
 if __name__ == '__main__':
     unittest.main()

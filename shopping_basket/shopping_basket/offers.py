2eaba3c7b56aebda0d02335e667ce28659f133d5
James Sandford <sandford.james@gmail.com>
2019-09-27 11:00:08 +0100

    started setting up a xfory offer

--- shopping_basket/shopping_basket/offers.py
+++ shopping_basket/shopping_basket/offers.py
@@ -1,11 +1,22 @@
 #To be implemented by other offers to add diffrent funcionality with each offer 
 class offer():
+    _multi = False
     #sets up the offer
-    def setup(self, multi = False, **kwargs):
+    def setup(self, **kwargs):
         pass
     #returns a float of the discount
     def discount(self,**kwargs):
         pass
     #returns if the offer is for multiple products or just one type
     def isMulti(self):
+        return self._multi
+
+#for offers where you get an amount for the pice of a diffrent amount, ie 3 for 2
+class xfory(offer):
+    _x = _y = 0
+    def setup(self, **kwargs):
+        self._x = kwargs["x"]
+        self._y = kwargs["y"]
+        self._multi = False
+    def discount(self, **kwargs):
         pass

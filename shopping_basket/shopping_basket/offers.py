d804059b7e650ed4c8acf050793d4deec63e3c85
James Sandford <sandford.james@gmail.com>
2019-09-27 14:27:41 +0100

    added comments on the offers

--- shopping_basket/shopping_basket/offers.py
+++ shopping_basket/shopping_basket/offers.py
@@ -14,11 +14,17 @@ class offer():
 #for offers where you get an amount for the pice of a diffrent amount, ie 3 for 2
 class xfory(offer):
     _x = _y = 0
+    #kwargs = (x:int,y:int) 
+	#x being the amount needed for the discount and the y being the amout the customor pays for.
+	#ie. 3 for 2 , x would be 3 and y would be 2
     def __init__(self, **kwargs):
         self._x = kwargs["x"]
         self._y = kwargs["y"]
         self._multi = False
-    #kwargs are amount, cost
+    #kwargs = (amount:int,cost:float) 
+	#amount is the amount being bought
+	#cost is cost per item
+    #returns a float
     def discount(self, **kwargs):
         amount = kwargs["amount"]
         cost = kwargs["cost"]
@@ -28,9 +34,15 @@ class xfory(offer):
 
 class percent(offer):
     _percent = 0
+    #kwarfgs = percent:int
+	#percent is a whole number > 0
     def __init__(self, **kwargs):
         self._percent = kwargs["percent"]
         self._multi = False
+    #kwargs = (amount:int,cost:float) 
+	#amount is the amount being bought
+	#cost is cost per item
+    #returns a float
     def discount(self, **kwargs):
         amount = kwargs["amount"]
         cost = kwargs["cost"]
